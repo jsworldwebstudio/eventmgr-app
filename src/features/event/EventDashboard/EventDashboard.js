082e66dc8fc5a724a8af4ad3d1f265b978ce2230
James Slaughter <james.slaughter08@gmail.com>
2019-02-21 14:21:47 -0600

    end of section 5

--- src/features/event/EventDashboard/EventDashboard.js
+++ src/features/event/EventDashboard/EventDashboard.js
@@ -8,7 +8,7 @@ const eventsData = [
   {
     id: '1',
     title: 'Trip to Tower of London',
-    date: '2018-03-27T11:00:00+00:00',
+    date: '2018-03-27',
     category: 'culture',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
@@ -32,7 +32,7 @@ const eventsData = [
   {
     id: '2',
     title: 'Trip to Punch and Judy Pub',
-    date: '2018-03-28T14:00:00+00:00',
+    date: '2018-03-28',
     category: 'drinks',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
@@ -58,15 +58,40 @@ const eventsData = [
 class EventDashboard extends Component {
   state = {
     events: eventsData,
-    isOpen: false
+    isOpen: false,
+    selectedEvent: null
   }
 
   handleFormOpen = () => {
-    this.setState({ isOpen: true })
+    this.setState({
+      selectedEvent: null,
+      isOpen: true
+    });
   };
 
   handleCancel = () => {
     this.setState({ isOpen: false })
+  };
+
+  handleUpdateEvent = (updatedEvent) => {
+    this.setState({
+      events: this.state.events.map(event => {
+        if (event.id === updatedEvent.id) {
+          return Object.assign({}, updatedEvent);
+        } else {
+          return event
+        }
+      }),
+      isOpen: false,
+      selectedEvent: null
+    });
+  };
+
+  handleOpenEvent = (eventToOpen) => () => {
+    this.setState({
+      selectedEvent: eventToOpen,
+      isOpen: true
+    });
   };
 
   handleCreateEvent = (newEvent) => {
@@ -80,17 +105,30 @@ class EventDashboard extends Component {
     console.log(this.state.events);
   };
 
+  handleDeleteEvent = (eventID) => () => {
+    const updatedEvents = this.state.events.filter(e => e.id !== eventID);
+    this.setState({
+      events: updatedEvents
+    });
+  };
+
   render() {
-    const { events, isOpen } = this.state;
+    const { events, isOpen, selectedEvent } = this.state;
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList events={events} />
+          <EventList
+            deleteEvent={this.handleDeleteEvent}
+            onEventOpen={this.handleOpenEvent}
+            events={events}
+          />
         </Grid.Column>
         <Grid.Column width={6}>
           <Button onClick={this.handleFormOpen} positive content="Create Event" />
           {isOpen && (
             <EventForm
+              updateEvent={this.handleUpdateEvent}
+              selectedEvent={selectedEvent}
               createEvent={this.handleCreateEvent}
               handleCancel={this.handleCancel}
             />
